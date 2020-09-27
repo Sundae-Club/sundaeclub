abd3393e803c5ac6e6228fd221a5b66f4094d787
Max Shelley <max@measuredbrilliance.com>
2020-09-27 10:57:15 +0100

    Add edit and update actions to ChannelsController
    This is an initial version of our ChannelsController and some rough UI
    in order to be able to edit a channel.
    
    We also added some Tailwind CSS classes to the #show view, mainly to
    check they were working and make the UI a bit more useable during
    development.

--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -4,7 +4,23 @@ class ChannelsController < ApplicationController
   def show
   end
 
+  def edit
+  end
+
+  def update
+    if @channel.update!(channel_params)
+      flash['notice'] = "#{@channel.name} has been updated successfully."
+      redirect_to channel_path(@channel)
+    else
+      render :edit
+    end
+  end
+
   private
+
+  def channel_params
+    params.require(:channel).permit(:name, :description)
+  end
 
   def set_channel
     @channel = Channel.find(params[:id])
