c01545eb9c62c7e67d6858c5a6fce663bf0a5404
Max Shelley <max@measuredbrilliance.com>
2020-10-11 11:08:39 +0100

    Add config for test and dev environments as part of devise installation
    Devise needs to be able to send emails and we also need to be able to
    test that, so this commit adds the basic config to enable that enable
    that.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,4 +59,8 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # Adding in `default_url_options` as part of the Devise installation,
+  # although it may well be needed for action mailer too.
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
