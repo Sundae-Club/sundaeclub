b6f753d3d5142f84194fe1eddaba3cc1beee7183
Max Shelley <max@measuredbrilliance.com>
2020-10-11 11:08:39 +0100

    Add config for test and dev environments as part of devise installation
    Devise needs to be able to send emails and we also need to be able to
    test that, so this commit adds the basic config to enable that enable
    that.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -40,6 +40,9 @@ Rails.application.configure do
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.default_options = { from: 'hello@sundaeclub.co' }
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
