38d78913c879c970ac5fdac264eb88fa6305f755
Max Shelley <max@measuredbrilliance.com>
2020-10-11 10:19:13 +0100

    Add channel suspensions controller in control panel
    In this commit we're introducing a new 'suspensions' controller into
    the recently added 'controlpanel' namespace, where we're implementing
    'admin' functionality.
    
    'Suspension' here was used as example during a stream of something an
    admin would want to do, so the feature isn't implemented beyond this.
    
    You can see the stream where this was added here:
    https://youtu.be/TbRw2jHpgg0

---
+++ app/controllers/control_panel/channels/suspensions_controller.rb
@@ -0,0 +1,15 @@
+class ControlPanel::Channels::SuspensionsController < ApplicationController
+  before_action :set_channel
+
+  def create
+    @channel.update(suspended_at: Time.zone.now)
+    flash['notice'] = "#{@channel.name} has been suspended."
+    redirect_to control_panel_channel_path(@channel)
+  end
+
+  private
+
+  def set_channel
+    @channel = Channel.find(params[:channel_id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   namespace :control_panel, path: 'controlpanel' do
-    resources :channels, only: :show
+    resources :channels, only: :show do
+      resource :suspension, only: :create, module: :channels
+    end
   end
 
   resources :channels, only: [:show, :edit, :update]
