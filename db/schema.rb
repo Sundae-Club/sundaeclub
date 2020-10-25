6281630a1c2a6aa5477bd2ff1a727d5036dccde9
Max Shelley <max@measuredbrilliance.com>
2020-10-25 11:55:23 +0000

    Add Site model
    We initially planned on a user creating a Channel and using that as the
    primary resource we would use to create a site. As we thought about it,
    we realised we may want to add multiple other channels and have other
    content on the site. This Site model is intended to represent this
    instead.
    
    Here's a link to the livestream where we looked at planning these
    changes: https://youtu.be/YDS_DKsCXyo

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_11_101347) do
+ActiveRecord::Schema.define(version: 2020_10_25_115207) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,16 @@ ActiveRecord::Schema.define(version: 2020_10_11_101347) do
     t.text "slug"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "sites", force: :cascade do |t|
+    t.text "name"
+    t.text "description"
+    t.text "slug"
+    t.bigint "organisation_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["organisation_id"], name: "index_sites_on_organisation_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -54,4 +64,5 @@ ActiveRecord::Schema.define(version: 2020_10_11_101347) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "sites", "organisations"
 end
