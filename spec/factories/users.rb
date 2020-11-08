32bca78d1ef66dfe1d8f50dc6dbde7d16ffc2c3f
Max Shelley <max@measuredbrilliance.com>
2020-11-08 10:19:19 +0000

    Fix tests requiring a signed-in confirmed user
    This test was broken in earlier work where we added Devise to the base
    controller, so we needed to auth the user in the test. After adding a
    signed-in user, the test initially failed as the user wasn't
    'confirmed'. To resolve this, we're adding a trait to the User factory
    and using this to update the failing tests.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,5 +2,9 @@ FactoryBot.define do
   factory :user do
     sequence(:email) { |n| "user.num#{n}@example.com" }
     password 'password'
+
+    trait :confirmed do
+      confirmed_at Time.current
+    end
   end
 end
