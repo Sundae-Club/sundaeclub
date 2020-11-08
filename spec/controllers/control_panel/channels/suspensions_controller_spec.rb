72c7a70a46dd7c2f84878ace09861e2726121202
Max Shelley <max@measuredbrilliance.com>
2020-11-08 10:19:19 +0000

    Fix tests requiring a signed-in confirmed user
    This test was broken in earlier work where we added Devise to the base
    controller, so we needed to auth the user in the test. After adding a
    signed-in user, the test initially failed as the user wasn't
    'confirmed'. To resolve this, we're adding a trait to the User factory
    and using this to update the failing tests.

--- spec/controllers/control_panel/channels/suspensions_controller_spec.rb
+++ spec/controllers/control_panel/channels/suspensions_controller_spec.rb
@@ -6,9 +6,13 @@ describe ControlPanel::Channels::SuspensionsController, type: :controller do
   let(:channel) { FactoryBot.create(:channel) }
 
   context '#create' do
-    it 'should suspend the channel' do
-      post :create, params: { channel_id: channel.id }
-      expect(channel.reload.suspended_at).to_not be_nil
+    context 'with a confirmed, signed-in user' do
+      before { sign_in FactoryBot.create(:user, :confirmed) }
+
+      it 'should suspend the channel' do
+        post :create, params: { channel_id: channel.id }
+        expect(channel.reload.suspended_at).to_not be_nil
+      end
     end
   end
 end
--- spec/controllers/control_panel/channels_controller_spec.rb
+++ spec/controllers/control_panel/channels_controller_spec.rb
@@ -6,9 +6,13 @@ describe ControlPanel::ChannelsController, type: :controller do
   let(:channel) { FactoryBot.create(:channel) }
 
   context '#show' do
-    it 'should return a successful response' do
-      get :show, params: { id: channel.id }
-      expect(response).to have_http_status(:ok)
+    context 'with a confirmed, signed-in user' do
+      before { sign_in FactoryBot.create(:user, :confirmed) }
+
+      it 'should return a successful response' do
+        get :show, params: { id: channel.id }
+        expect(response).to have_http_status(:ok)
+      end
     end
   end
 end
