f6bba85b197d66f67c7e8c769f4b148ca0f92327
Max Shelley <max@measuredbrilliance.com>
2020-11-15 11:53:18 +0000

    Rename stylesheets in webpack and update head tags
    When trying to get GitHub Actions and Heroku set up, we found that our
    initial setting for webpack didn't seem to work. I'm not 100% sure
    of the details here as I didn't take the time to look into it, but this
    combination seemed to get things working.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,4 +15,4 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
-require("css/application.scss")
+require("stylesheets/application.scss")
