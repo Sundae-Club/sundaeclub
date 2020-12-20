c1fb091ded641c73b04c718a6edd702353d54be2
Max Shelley <max@measuredbrilliance.com>
2020-12-20 11:10:56 +0000

    Add before action loading site in site_setup namespace

--- app/controllers/sites_setup/base_controller.rb
+++ app/controllers/sites_setup/base_controller.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 class SitesSetup::BaseController < ApplicationController
+  before_action :set_site
+
+  private
+
+  def set_site
+    @site = Site.find(params[:site_id])
+  end
 end
