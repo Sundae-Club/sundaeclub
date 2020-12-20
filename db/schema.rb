89aa5f0a58ead33c52fcf00f8ffb79894816a2c6
Max Shelley <max@measuredbrilliance.com>
2020-12-20 11:50:29 +0000

    WIP: Add delayed_job installation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_20_110745) do
+ActiveRecord::Schema.define(version: 2020_12_20_114929) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,21 @@ ActiveRecord::Schema.define(version: 2020_12_20_110745) do
     t.bigint "site_id", null: false
     t.text "youtube_channel_id"
     t.index ["site_id"], name: "index_channels_on_site_id"
+  end
+
+  create_table "delayed_jobs", force: :cascade do |t|
+    t.integer "priority", default: 0, null: false
+    t.integer "attempts", default: 0, null: false
+    t.text "handler", null: false
+    t.text "last_error"
+    t.datetime "run_at"
+    t.datetime "locked_at"
+    t.datetime "failed_at"
+    t.string "locked_by"
+    t.string "queue"
+    t.datetime "created_at", precision: 6
+    t.datetime "updated_at", precision: 6
+    t.index ["priority", "run_at"], name: "delayed_jobs_priority"
   end
 
   create_table "organisations", force: :cascade do |t|
